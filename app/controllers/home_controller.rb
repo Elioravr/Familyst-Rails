736bf25a00d19c3b87096284873b4579d8573b21
Elior Avramovitz <elioravr@gmail.com>
2015-01-09 01:39:44 +0200

    Make the root page of a login user to the lists_index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     unless user_signed_in?
       redirect_to new_user_session_path
     else
-      redirect_to todos_path
+      redirect_to lists_path
     end
   end
 end
